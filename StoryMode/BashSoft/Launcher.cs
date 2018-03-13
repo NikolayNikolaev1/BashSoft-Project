50a8c3a0707ea36a29d550d8fd1533f3f1183103
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-03-13 15:58:34 +0200

    Added methods to the new class Tester.cs

--- StoryMode/BashSoft/Launcher.cs
+++ StoryMode/BashSoft/Launcher.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace BashSoft
+﻿namespace BashSoft
 {
     class Launcher
     {
@@ -8,10 +6,12 @@ namespace BashSoft
         {
             //IOManager.TraverseDirectory(@"C:\Users\user\Source\Repos");
 
-            StudentsRepository.InitializeData();
-            StudentsRepository.GetAllStudentsFromCourse("Unity");
-            OutputWriter.WriteEmptyLine();
-            StudentsRepository.GetStudentScoresFromCourse("Unity", "Ivan");
+            //StudentsRepository.InitializeData();
+            //StudentsRepository.GetAllStudentsFromCourse("Unity");
+            //OutputWriter.WriteEmptyLine();
+            //StudentsRepository.GetStudentScoresFromCourse("Unity", "Ivan");
+
+            Tester.CompareContent(@"C:\Users\user\Desktop\Нова папка (4)\BashSoft-FirstWeek\test2.txt", @"C:\Users\user\Desktop\Нова папка (4)\BashSoft-FirstWeek\test3.txt");
         }
     }
 }
