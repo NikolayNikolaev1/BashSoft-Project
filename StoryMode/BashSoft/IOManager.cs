a4a0ba0914ec52c9a1e8f96d5ddc9ed1cfef9e5a
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-03-13 16:13:08 +0200

    Modified TraverseDirectory method.

--- StoryMode/BashSoft/IOManager.cs
+++ StoryMode/BashSoft/IOManager.cs
@@ -8,14 +8,19 @@ namespace BashSoft
         public static void TraverseDirectory(string path)
         {
             OutputWriter.WriteEmptyLine();
-            int initialIdentation = path.Split('\\').Length;
+            int initialIdentation = SessionData.currentPath.Split('\\').Length;
             Queue<string> subFolders = new Queue<string>();
-            subFolders.Enqueue(path);
+            subFolders.Enqueue(SessionData.currentPath);
 
             while (subFolders.Count != 0)
             {
                 string currentPath = subFolders.Dequeue();
                 int identation = currentPath.Split('\\').Length - initialIdentation;
+
+                if (depth - identation < 0)
+                {
+                    break;
+                }
 
                 foreach (var directoryPath in Directory.GetDirectories(currentPath))
                 {
@@ -23,6 +28,13 @@ namespace BashSoft
                 }
 
                 OutputWriter.WriteMessageOnNewLine(string.Format("{0}{1}", new string('-', identation), currentPath));
+
+                foreach (var file in Directory.GetFiles(currentPath))
+                {
+                    int indexOfLastSlash = file.LastIndexOf("\\");
+                    string fileName = file.Substring(indexOfLastSlash);
+                    OutputWriter.WriteMessageOnNewLine(new string('-', indexOfLastSlash) + fileName);
+                }
             }
         }
 
