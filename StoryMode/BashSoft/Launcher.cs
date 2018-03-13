a8c0ed6f55f66001061c25eae473629f34a94509
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-03-13 17:47:54 +0200

    Ordered Classes into Folders.

--- StoryMode/BashSoft/Launcher.cs
+++ StoryMode/BashSoft/Launcher.cs
@@ -4,16 +4,7 @@
     {
         static void Main(string[] args)
         {
-            //IOManager.TraverseDirectory(@"C:\Users\user\Source\Repos");
-
-            //StudentsRepository.InitializeData();
-            //StudentsRepository.GetAllStudentsFromCourse("Unity");
-            //OutputWriter.WriteEmptyLine();
-            //StudentsRepository.GetStudentScoresFromCourse("Unity", "Ivan");
-
-            //Tester.CompareContent(@"C:\Users\user\Desktop\Нова папка (4)\BashSoft-FirstWeek\test2.txt", @"C:\Users\user\Desktop\Нова папка (4)\BashSoft-FirstWeek\test3.txt");
-
-            //IOManager.CreateDirectoryInCurrentFolder("pesho");
+            InputReader.StartReadingCommands();
         }
     }
 }
