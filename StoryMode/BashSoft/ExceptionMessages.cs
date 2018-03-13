23cb8b05cef72f323d8fd27c5e78436f08f78f02
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-03-13 15:12:45 +0200

    Added methods to the StudentsRepository class

--- StoryMode/BashSoft/ExceptionMessages.cs
+++ StoryMode/BashSoft/ExceptionMessages.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace BashSoft
+﻿namespace BashSoft
 {
-    class ExceptionMessages
+    public static class ExceptionMessages
     {
+        public const string DataAlreadyInitialisedException = "Data is already initialized!";
+
+        public const string DataNotInitializedExceptionMessage = "The data structure must be initialised first in order to make any operations with it.";
+
+        public const string InexistingCourseInDataBase = "The course you are trying to get does not exist in the data base!";
+
+        public const string InexistingStudentInDataBase = "The user name for the student you are trying to get does not exist!";
+
+
     }
 }
--- StoryMode/BashSoft/StudentsRepository.cs
+++ StoryMode/BashSoft/StudentsRepository.cs
@@ -1,10 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace BashSoft
 {
-    class StudentsRepository
+    public static class StudentsRepository
     {
+        public static bool isDataInitialized = false;
+        private static Dictionary<string, Dictionary<string, List<int>>> studentByCourse;
+
+        public static void InitializeData()
+        {
+            if (!isDataInitialized)
+            {
+                OutputWriter.WriteMessageOnNewLine("Reading data...");
+                studentByCourse = new Dictionary<string, Dictionary<string, List<int>>>();
+                ReadData();
+            }
+            else
+            {
+                OutputWriter.DisplayException(ExceptionMessages.DataAlreadyInitialisedException);
+            }
+        }
+
+        private static void ReadData()
+        {
+            string input = Console.ReadLine();
+
+            while (!string.IsNullOrEmpty(input))
+            {
+                string[] tokens = input.Split();
+                string course = tokens[0];
+                string student = tokens[1];
+                int mark = int.Parse(tokens[2]);
+
+                if (!studentByCourse.ContainsKey(course))
+                {
+                    studentByCourse.Add(course, new Dictionary<string, List<int>>());
+                }
+
+                if (!studentByCourse[course].ContainsKey(student))
+                {
+                    studentByCourse[course].Add(student, new List<int>());
+                }
+
+                studentByCourse[course][student].Add(mark);
+                input = Console.ReadLine();
+            }
+
+            isDataInitialized = true;
+            OutputWriter.WriteMessageOnNewLine("Data read!");
+        }
+
+        private static bool IsQueryForCoursePossible(string courseName)
+        {
+            if (isDataInitialized)
+            {
+                if (studentByCourse.ContainsKey(courseName))
+                {
+                    return true;
+                }
+                else
+                {
+                    OutputWriter.DisplayException(ExceptionMessages.InexistingCourseInDataBase);
+                }
+            }
+            else
+            {
+                OutputWriter.DisplayException(ExceptionMessages.DataNotInitializedExceptionMessage);
+            }
+
+            return false;
+        }
+
+        private static bool IsQueryForStudentPossible(string courseName, string studentUserName)
+        {
+            if (IsQueryForCoursePossible(courseName) 
+                && studentByCourse[courseName].ContainsKey(studentUserName))
+            {
+                return true;
+            }
+            else
+            {
+                OutputWriter.DisplayException(ExceptionMessages.InexistingStudentInDataBase);
+            }
+
+            return false;
+        }
     }
 }
