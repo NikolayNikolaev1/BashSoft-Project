c7e76403401171fb82c64df339a2ed0de007f4ea
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-05-21 18:16:44 +0300

    Creating the ShowCourseCommand.

---
+++ StoryMode/BashSoft/IO/Commands/ShowCourseCommand.cs
@@ -0,0 +1,32 @@
+﻿using BashSoft.Exceptions;
+
+namespace BashSoft.IO.Commands
+{
+    public class ShowCourseCommand : Command
+    {
+        public ShowCourseCommand(
+            string input, string[] data, Tester judge, StudentsRepository repository, IOManager inputOutputManager) 
+            : base(input, data, judge, repository, inputOutputManager)
+        {
+        }
+
+        public override void Execute()
+        {
+            if (this.Data.Length == 2)
+            {
+                string course = this.Data[1];
+                this.Repository.GetAllStudentsFromCourse(course);
+            }
+            else if (this.Data.Length == 3)
+            {
+                var course = this.Data[1];
+                var username = this.Data[2];
+                this.Repository.GetStudentScoresFromCourse(course, username);
+            }
+            else
+            {
+                throw new InvalidCommandException(this.Input);
+            }
+        }
+    }
+}
