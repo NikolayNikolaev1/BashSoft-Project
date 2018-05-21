8124310678b5adf6bb0f5b3259d025534b0a3740
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-05-21 16:11:02 +0300

    Making Exceptions for the IOManager and Tester.

--- StoryMode/BashSoft/IO/IOManager.cs
+++ StoryMode/BashSoft/IO/IOManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BashSoft.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -56,7 +57,7 @@ namespace BashSoft
             }
             catch (ArgumentException)
             {
-                throw new ArgumentException(ExceptionMessages.ForbiddenSymbolsContainedInName);
+                throw new InvalidFileNameException();
             }
         }
 
