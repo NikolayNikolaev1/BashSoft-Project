b10c711f0f8799a434d3097682d0344d44b708f9
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-05-24 16:04:48 +0300

    Making the Inject Attribute.

--- StoryMode/BashSoft/Attributes/AliasAttribute.cs
+++ StoryMode/BashSoft/Attributes/AliasAttribute.cs
@@ -1,10 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace BashSoft.Attributes
 {
-    class AliasAttribute
+    [AttributeUsage(AttributeTargets.Class)]
+    public class AliasAttribute : Attribute
     {
+        private string name;
+
+        public AliasAttribute(string aliasName)
+        {
+            this.name = aliasName;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+
+        public override bool Equals(object obj) => this.name.Equals(obj);
     }
 }
